4715fec0eb79a3e6efada34bdc9400c8bee06b79
palkeshpatel <patel.palkesh@gmail.com>
2025-12-05 17:50:45 +0530

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\CacheController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\Master\CategoryController;
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/login');
 });
+
+// Cache clear route - Access via: /clear?token=YOUR_TOKEN
+Route::get('/clear', [CacheController::class, 'clear'])->name('cache.clear');
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
